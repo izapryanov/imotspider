d69eac5f6f2fc229850254fd61557ce367119387
¨izapryanov¨ <izapryanov@gmail.com>
2016-12-02 08:04:12 -0800

    Encoding fixed

--- imotSearch/spiders/imot_search.py
+++ imotSearch/spiders/imot_search.py
@@ -2,27 +2,27 @@ from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from imotSearch.items import ImotSearchItem
 import sys
-
+import logging
 
 
 class MySpider(BaseSpider):
     name = "imot_search"
     allowed_domains = ["www.google.com"]
     start_urls = ["https://www.google.com/search?client=ubuntu&channel=fs&q=%D0%B8%D0%BC%D0%BE%D1%82%D0%B8+%D1%81%D0%BE%D1%84%D0%B8%D1%8F+%D0%BA%D1%8A%D1%89%D0%B0+%D0%B4%D1%80%D0%B0%D0%B3%D0%B0%D0%BB%D0%B5%D0%B2%D1%86%D0%B8+&ie=utf-8&oe=utf-8&gfe_rd=cr&ei=52QxWO3QO7DY8AfN46LgAw"]
-
+    logging.basicConfig(filename='imot.log',level=logging.DEBUG)
     def parse(self, response):
-	reload(sys)
-	sys.setdefaultencoding('utf-8')
+        reload(sys)
+        sys.setdefaultencoding('utf-8')
         hxs = HtmlXPathSelector(response)
         titles = hxs.select("//h3")
-	items = []
+        items = []
         for element in titles:
-	    item = ImotSearchItem()
+            item = ImotSearchItem()
             titles = element.select("a/text()").extract()
-	    t = ''.join(titles)
-	    title = t.decode('unicode-escape')
-	    item["title"] = title 
+            logging.debug("Titles: " + ",".join(titles))
+            t = ''.join(titles)
+            title = t
+            item["title"] = title
             item["link"] = element.select("a/@href").extract()
-	    items.append(item)       	
-	return items
-
+            items.append(item)
+        return items
